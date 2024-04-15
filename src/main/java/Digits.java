63b4209cce4a3c3509ca9afe3d108b7b3d9776a0
Brian0520 <143020505+Brian0520@users.noreply.github.com>
2024-04-15 13:26:07 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -8,7 +8,7 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	digitList = new ArrayList <>();
+	digitList = new ArrayList <Integer>();
 	if(num == 0){
 		digitList.add(0);
 	}else{
@@ -22,7 +22,7 @@ public class Digits
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
 		for(int i = 0; i < digitList.size()-1;i++)
-			if(digitList.get(i) > digitList.get(i+1))
+			if(digitList.get(i) >= digitList.get(i+1))
 				return false;
 		return true;
 	}
